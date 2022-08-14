e41377846eb0f30e9914c0bc39fcf89f877151cc
kok-s0s <2694308562@qq.com>
2022-08-14 17:17:49 +0800

    feat(data-structure): day-1(Array)

---
+++ LeetCode/Data-Structure/53.maximum-subarray.cpp
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=53 lang=cpp
+ *
+ * [53] Maximum Subarray
+ */
+
+// @lc code=start
+class Solution {
+public:
+  int maxSubArray(vector<int> &nums) {
+    int max_sum = nums[0];
+    int cur_sum = nums[0];
+    for (int i = 1; i < nums.size(); i++) {
+      cur_sum = max(nums[i], cur_sum + nums[i]);
+      max_sum = max(max_sum, cur_sum);
+    }
+    return max_sum;
+  }
+};
+// @lc code=end
---
+++ LeetCode/Data-Structure/README.md
@@ -0,0 +1,7 @@
+# [Data Structure](https://leetcode.com/study-plan/data-structure/?progress=xh9apjum)
+
+## Day 1 Array
+
+- [217. Contains Duplicate](./217.contains-duplicate.cpp)
+
+- [53. Maximum Subarray](./53.maximum-subarray.cpp)
