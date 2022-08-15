6cacdbefc6cfdfb4b3462c25c783bc28c2b377b9
kok-s0s <2694308562@qq.com>
2022-08-16 07:48:15 +0800

    feat(data-structure): day-2(Array)

---
+++ LeetCode/Data-Structure/1.two-sum.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=1 lang=cpp
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+class Solution {
+public:
+  vector<int> twoSum(vector<int> &nums, int target) {
+    unordered_map<int, int> hash;
+    vector<int> result;
+    for (int i = 0; i < nums.size(); ++i) {
+      int numberToFind = target - nums[i];
+      if (hash.find(numberToFind) != hash.end()) {
+        result.push_back(hash[numberToFind]);
+        result.push_back(i);
+        return result;
+      }
+      hash[nums[i]] = i;
+    }
+    return result;
+  }
+};
+// @lc code=end
--- LeetCode/Data-Structure/README.md
+++ LeetCode/Data-Structure/README.md
@@ -5,3 +5,9 @@
 - [217. Contains Duplicate](./217.contains-duplicate.cpp)
 
 - [53. Maximum Subarray](./53.maximum-subarray.cpp)
+
+## Day 2 Array
+
+- [1. Two Sum](./1.two-sum.cpp)
+
+- [88. Merge Sorted Array](./88.merge-sorted-array.cpp)
