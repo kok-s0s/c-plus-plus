e6ff3aad97a6ceebd9132b1bfe4c43af02b4cf0c
kok-s0s <2694308562@qq.com>
2022-08-21 16:04:31 +0800

    feat(data-structure): day-3(Array)

---
+++ LeetCode/Data-Structure/350.intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode id=350 lang=cpp
+ *
+ * [350] Intersection of Two Arrays II
+ */
+
+// @lc code=start
+class Solution {
+public:
+  vector<int> intersect(vector<int> &nums1, vector<int> &nums2) {
+    // using Map & without sort
+    // map<int, int> freq;
+    // vector<int> res;
+    // for (int i = 0; i < nums1.size(); ++i) {
+    //   freq[nums1[i]]++;
+    // }
+    // for (int i = 0; i < nums2.size(); ++i) {
+    //   if (freq[nums2[i]] > 0) {
+    //     freq[nums2[i]]--;
+    //     res.push_back(nums2[i]);
+    //   }
+    // }
+    // return res;
+
+    // Two pointer with (x, y) with sort
+    sort(nums1.begin(), nums1.end());
+    sort(nums2.begin(), nums2.end());
+    int x = 0, y = 0;
+    vector<int> res;
+    while (x < nums1.size() && y < nums2.size()) {
+      if (nums1[x] == nums2[y]) {
+        res.push_back(nums1[x]);
+        x++;
+        y++;
+      } else if (nums1[x] < nums2[y]) {
+        x++;
+      } else {
+        y++;
+      }
+    }
+    return res;
+  }
+};
+// @lc code=end
--- LeetCode/Data-Structure/README.md
+++ LeetCode/Data-Structure/README.md
@@ -11,3 +11,9 @@
 - [1. Two Sum](./1.two-sum.cpp)
 
 - [88. Merge Sorted Array](./88.merge-sorted-array.cpp)
+
+## Day 3 Array
+
+- [350. Intersection of Two Arrays II](./350.intersection-of-two-arrays-ii.cpp)
+
+- [121. Best Time to Buy and Sell Stock](./121.best-time-to-buy-and-sell-stock.cpp)
